8afb25d619e76d38bcef2a422291b2cbb547e063
Chao Xu <xux1912@gmail.com>
2016-08-02 15:05:48 -0500

    Update snp_preprocessing.sh

--- bin/snp_preprocessing.sh
+++ bin/snp_preprocessing.sh
@@ -6,3 +6,4 @@
 ##       $2 -- LD threshold for pruning (0, 1)
 
 plink2 --file hapmap3 --extract $1 --keep CEU_hapmap --indep-pairwise 50 5 $2 --r2 inter-chr with-freqs --ld-window-r2 0 --out uppro
+plink2 --bfile up_pro --extract up_pro.prune.in --make-bed --out up_pro_pruned
